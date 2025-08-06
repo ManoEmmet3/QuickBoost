67f3ce114b9c182f1448307e1afb768515b988ac
manoemmet3 <manoemmet3@gmail.com>
2025-08-06 19:30:51 -0300

    first commit

---
+++ quickboost.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+janela = tk.Tk()
+janela.title("Quickboost")
+janela.geometry("640x480")
+janela.configure(bg="#252526")
+janela.resizable(False, False)
+
+janela.iconbitmap("assets\\quick.ico")
+
+janela.mainloop()
